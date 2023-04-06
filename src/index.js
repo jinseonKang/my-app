3655aa330480f9bddbabc279823ba6851f8661b3
jinseonKang <boicekjs@naver.com>
2023-04-06 12:12:20 +0900

    new

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-import Library from './chapter_03/Library';
-import Clock from './chapter_04/Clock'
+import Library from "./chapter_03/Library";
+import Clock from "./chapter_04/Clock";
+import CommentList from "./chapter_05/CommentList";
+import NotificationList from "./chapter_06/NotificationList";
 
-setInterval(() => {
-  ReactDOM.render (
-    <React.StrictMode>
-      <Clock />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-}, 1000);
+ReactDOM.render(
+  <React.StrictMode>
+    <NotificationList />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
