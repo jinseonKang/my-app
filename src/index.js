d70b3055a55957b367f6acd43910c3a65c09e687
jinseonKang <boicekjs@naver.com>
2023-05-25 11:02:16 +0900

    push

--- src/index.js
+++ src/index.js
@@ -14,10 +14,12 @@ import LandingPage from "./chapter_09/LandingPage";
 import AttendanceBook from "./chapter_10/AttendanceBook";
 import SignUp from "./chapter_11/SignUp";
 import Calculator from "./chapter_12/Calculator";
+import ProfileCard from "./chapter_13/ProfileCard";
+import DarkOrLight from "./chapter_14/DarkOrLight";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Calculator />
+    <DarkOrLight />
   </React.StrictMode>,
   document.getElementById("root")
 );
